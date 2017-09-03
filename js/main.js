cd06c4c725d77464ea96fe5eccc147f4da70b383
Kasey Markham <kaseycm@gmail.com>
2017-09-02 22:27:04 -0400

    Complete task functionality created

--- js/main.js
+++ js/main.js
@@ -13,6 +13,23 @@ function deleteTask() {
   parent.removeChild(task);
 }
 
+function completeTask() {
+  var task = this.parentNode.parentNode;
+  var parent = task.parentNode;
+  var id = parent.id;
+
+  if (id === 'todo') {
+    target = document.getElementById('completed');
+  } else {
+    target = document.getElementById('todo');
+  }
+
+  parent.removeChild(task);
+  target.insertBefore(task, target.childNodes[0]);
+
+
+}
+
 function addTask(text) {
   var list = document.getElementById('todo');
 
@@ -28,7 +45,15 @@ function addTask(text) {
 
   deleteButton.addEventListener('click', deleteTask)
 
+  var completeButton = document.createElement('button');
+  completeButton.classList.add('delete');
+  completeButton.innerHTML = 'Complete';
+
+  completeButton.addEventListener('click', completeTask)
+
+
   buttons.appendChild(deleteButton);
+  buttons.appendChild(completeButton);
   task.appendChild(buttons);
 
   list.insertBefore(task, list.childNodes[0]);
