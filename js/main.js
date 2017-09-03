3b968ee0b54cce15e8b7bff4b11f342b1e61065c
Kasey Markham <kaseycm@gmail.com>
2017-09-02 22:07:51 -0400

    Delete task functionality created

--- js/main.js
+++ js/main.js
@@ -6,11 +6,30 @@ document.getElementById('add-button').addEventListener('click', function() {
   }
 });
 
+function deleteTask() {
+  var task = this.parentNode.parentNode;
+  var parent = task.parentNode;
+
+  parent.removeChild(task);
+}
+
 function addTask(text) {
   var list = document.getElementById('todo');
 
   var task = document.createElement('li');
   task.innerText = text;
+
+  var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+
+  var deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete');
+  deleteButton.innerHTML = 'Delete';
+
+  deleteButton.addEventListener('click', deleteTask)
+
+  buttons.appendChild(deleteButton);
+  task.appendChild(buttons);
 
   list.insertBefore(task, list.childNodes[0]);
 }
